b3a2f5ff4ae6b4ecd40edf71adb52f717b58586d
Liam T O'Toole <lotoole@uvm.edu>
2018-09-27 10:35:04 -0400

    finished all methods of player accept for checkforset

--- src/com/example/gofish/Player.java
+++ src/com/example/gofish/Player.java
@@ -1,8 +1,40 @@
 package com.example.gofish;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Player {
-    private ArrayList<Card> list = new ArrayList<Card>();
+    private ArrayList<Card> hand = new ArrayList<Card>();
+    private ArrayList<Card> sets = new ArrayList<Card>();
+    private String Name;
 
+    public Player(String name) {
+        Name = name;
+    }
+
+    public void addToHand(Card card) {
+        hand.add(card);
+    }
+
+    public void handToString() {
+        for(Card card : hand) {
+            System.out.println(card.toString());
+        }
+    }
+
+    private void checkForSet() {
+//        int[]
+        for(Card card : hand) {
+            int currentRank = card.getRank();
+
+        }
+    }
+
+    private void addCardToSet(Card card) {
+        sets.add(card);
+    }
+
+    private void removeCardFromHand(Card card) {
+        hand.remove(card);
+    }
 }
